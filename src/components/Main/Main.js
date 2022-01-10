ecc41f835b93101a147180856117b8bb3d65975e
ilzira murtazina <murtazina.ilzi@yandex.ru>
2022-01-10 16:40:33 +0500

    favicon

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,13 +2,15 @@ import './Main.css';
 
 function Main({onGetQuote, author, body}) {
     return (
-        <div className="main">
+        <section className="main">
+        <div className="main__container">
             <h1 className="main__header">quotes-maker</h1>
             <p className="main__text">press <span className="main__span">PUSH</span> to get the quote for the day</p>
             <button className="main__button" onClick={onGetQuote}>PUSH</button>
             <p className="main__quote">{author}</p>
             <p className="main__quote">{body}</p>
         </div>
+        </section>
     )
 }
 export default Main;
